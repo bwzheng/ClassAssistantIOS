cc3f1551b3e46bd07dee254e809c2fa036db4043
bwzheng <bob.zheng1998@gmail.com>
2017-02-18 16:50:56 -0800

    Initial Commit

---
+++ ClassAssistant/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ClassAssistant
+//
+//  Created by BOWEN ZHENG on 2/17/17.
+//  Copyright © 2017 BOWEN ZHENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
